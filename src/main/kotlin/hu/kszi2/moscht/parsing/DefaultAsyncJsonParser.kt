80e8ab19d75f4cb8c226b5b2b9c17d083087e776
bodand <bodand@proton.me>
2023-03-19 14:52:26 +0100

    fix: DefaultAsyncJsonParser

--- src/main/kotlin/hu/kszi2/moscht/parsing/DefaultAsyncJsonParser.kt
+++ src/main/kotlin/hu/kszi2/moscht/parsing/DefaultAsyncJsonParser.kt
@@ -1,9 +1,12 @@
 package hu.kszi2.moscht.parsing
 
 import hu.kszi2.moscht.Machine
+import kotlinx.coroutines.*
 
-class DefaultAsyncJsonParser(val blockingImpl: MachineJsonParser) : MachineAsyncJsonParser {
-    override suspend fun parseJson(payload: String): Machine {
-        TODO("Not yet implemented")
+class DefaultAsyncJsonParser(private val blockingImpl: MachineJsonParser = DefaultJsonParser()) : MachineAsyncJsonParser {
+    override suspend fun parseJson(payload: String): List<Machine> {
+        return withContext(Dispatchers.Default) {
+            return@withContext blockingImpl.parseJson(payload)
+        }
     }
 }
